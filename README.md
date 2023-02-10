8cf05f169187ed9e5b68509e817420d96941b0ff
Darlan Guimaraes <darlanibira@gmail.com>
2023-02-10 08:55:09 -0300

    add readme, change layout of buttons

---
+++ README.md
@@ -0,0 +1,5 @@
+# Construction of the project in Flutter by the administration of Casa Espírita Eurípedes Barsanulpho
+
+Deploy on Guthub Pages for PWA
+
+https://github.com/darlanguimaraes/ceeb-mobile
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "f31e4957b26fcc7b48f353957da6832a",
-"index.html": "8b2831f7825d7b7d921315372a25c078",
-"/": "8b2831f7825d7b7d921315372a25c078",
-"main.dart.js": "26659ef7ff5cfe95e2b66f59f7d9e4af",
+"index.html": "5042ded81d88339867029186498de3ac",
+"/": "5042ded81d88339867029186498de3ac",
+"main.dart.js": "f4d6ec5970cdab42cdda64d60ceb62be",
 "flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
 "favicon.png": "efa73b55a1077f6d194793ac86893bca",
 "icons/Icon-192.png": "854e789e7083fbaf28cee6fd9454b247",
